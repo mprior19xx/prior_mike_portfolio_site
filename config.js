78b0c97efeb512355fa01e9c6b326dedf8908fb3
mike prior <mprior19xx@gmail.com>
2019-12-08 01:28:27 -0500

    setting up heroku

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 const config = {
-    host: "localhost",
+    //LOCAL HOSTING
+    //host: "localhost",
+    //port: 3306, // 8889 for mac
+    //uname: "root",
+    //pword: "", // leave blank for windows
+    //database: "db_favourite-things"
+
+    //HEROKU HOSTING
+    host: "us-cdbr-iron-east-05.cleardb.net",
     port: 3306, // 8889 for mac
-    uname: "root",
-    pword: "", // leave blank for windows
-    database: "db_favourite-things"
+    uname: "b41b1b9bac6b5b",
+    pword: "a1e3e427", // leave blank for windows
+    database: "heroku_995a839e5397c44"
 }
 
 module.exports = config;
