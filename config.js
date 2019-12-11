3862cc0a097520acea28ab525ad95abc8b2c9869
mike prior <mprior19xx@gmail.com>
2019-12-11 13:08:47 -0500

    back to heroku

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const config = {
-    // //LOCAL HOSTING
-    //  host: "localhost",
-    //  port: 3306, // 8889 for mac
-    //  uname: "root",
-    //  pword: "", // leave blank for windows
-    //  database: "db_favourite-things"
+     //LOCAL HOSTING
+      //host: "localhost",
+      //port: 3306, // 8889 for mac
+      //uname: "root",
+      //pword: "", // leave blank for windows
+      //database: "db_favourite-things"
 
     //HEROKU HOSTING
-     host: "us-cdbr-iron-east-05.cleardb.net",
+    host: "us-cdbr-iron-east-05.cleardb.net",
     port: 3306, // 8889 for mac
-     uname: "b41b1b9bac6b5b",
-     pword: "a1e3e427", // leave blank for windows
+    uname: "b41b1b9bac6b5b",
+    pword: "a1e3e427", // leave blank for windows
     database: "heroku_995a839e5397c44"
 }
 
